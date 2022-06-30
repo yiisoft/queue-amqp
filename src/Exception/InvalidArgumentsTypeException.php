03647b6b160c48074b6f93484cc1df41ebcb0bd6
Nikita Zarubin <nikitadimitrov@gmail.com>
2022-06-30 10:52:30 +0300

    Fix #1: Create local exception classes (#48)
    Co-authored-by: Nikita Zarubin <n.zarubin@itcomgk.ru>

---
+++ src/Exception/InvalidArgumentsTypeException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Queue\AMQP\Exception;
+
+
+use InvalidArgumentException;
+use PhpAmqpLib\Wire\AMQPTable;
+
+class InvalidArgumentsTypeException extends InvalidArgumentException
+{
+    protected $message = '"arguments" parameter must be either an array or an ' . AMQPTable::class . ' object.';
+}
