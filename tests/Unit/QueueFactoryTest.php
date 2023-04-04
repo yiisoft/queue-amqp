645b6de363c20a8898184aca5c46cc0185ed4342
s1lver <s1lverhron@gmail.com>
2023-04-04 15:06:13 +0300

    (#5) Added more tests

---
+++ tests/Unit/QueueFactoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Queue\AMQP\Tests\Unit;
+
+use Yiisoft\Injector\Injector;
+use Yiisoft\Yii\Queue\AMQP\Tests\Integration\TestCase;
+use Yiisoft\Yii\Queue\Middleware\CallableFactory;
+use Yiisoft\Yii\Queue\QueueFactory;
+
+class QueueFactoryTest extends TestCase
+{
+    public function testSameChannelName(): void
+    {
+        $queue = $this->getQueue();
+        $container = $this->getContainer();
+        $factory = new QueueFactory(
+            [],
+            $queue,
+            $container,
+            new CallableFactory($container),
+            new Injector($container),
+            true,
+            $this->getAdapter()
+        );
+
+        self::assertEquals('yii-queue', $factory->get('yii-queue')->getChannelName());
+    }
+}
