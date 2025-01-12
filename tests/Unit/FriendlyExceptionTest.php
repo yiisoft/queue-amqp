6aa0bd19ec0d2caa13b39fed475a8f1d8b620f5e
viktorprogger <viktorprogger@gmail.com>
2025-01-13 01:10:28 +0500

    Bugfixes

--- tests/Unit/FriendlyExceptionTest.php
+++ tests/Unit/FriendlyExceptionTest.php
@@ -5,21 +5,9 @@ declare(strict_types=1);
 namespace Yiisoft\Queue\AMQP\Tests\Unit;
 
 use Yiisoft\Queue\AMQP\Exception\ExchangeDeclaredException;
-use Yiisoft\Queue\AMQP\Exception\NoKeyInPayloadException;
 
 final class FriendlyExceptionTest extends UnitTestCase
 {
-    public function testNoKeyInPayloadException(): void
-    {
-        $exception = new NoKeyInPayloadException(
-            'test',
-            ['item1', 'item2']
-        );
-
-        self::assertSame('No key "test" in payload', $exception->getName());
-        $this->assertMatchesRegularExpression('/test/', $exception->getSolution());
-    }
-
     public function testExchangeDeclaredException(): void
     {
         $exception = new ExchangeDeclaredException();
