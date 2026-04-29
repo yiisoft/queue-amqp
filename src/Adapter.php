31b644b22b6911bd80ce0ee3e47841d0618ba533
TiaNex <service@tianex.com>
2026-04-29 14:09:19 +0800

    Update Adapter.php

--- src/Adapter.php
+++ src/Adapter.php
@@ -10,7 +10,7 @@ use Throwable;
 use Yiisoft\Queue\Adapter\AdapterInterface;
 use Yiisoft\Queue\AMQP\Exception\NotImplementedException;
 use Yiisoft\Queue\Cli\LoopInterface;
-use Yiisoft\Queue\JobStatus;
+use Yiisoft\Queue\MessageStatus;
 use Yiisoft\Queue\Message\MessageInterface;
 use Yiisoft\Queue\Message\MessageSerializerInterface;
 
